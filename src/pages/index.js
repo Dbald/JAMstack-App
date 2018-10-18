271f61f993f232cb9d2bb5b0bd2aa0f2e3c4dc3e
itsdevinci <devinbaldwin315@gmail.com>
2018-10-18 00:21:03 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello World!</h1>
+    <p>Welcome to my Gatsby site.</p>
+    <p>I'm Devin Baldwin, I'm ready to create <br/>
+    something AWESOME!</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
